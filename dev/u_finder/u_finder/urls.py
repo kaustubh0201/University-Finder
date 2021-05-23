7ce98490f1fd1eded8b22100c7fe8fbb1603fb3e
kaustubh0201 <kaustubh1818@gmail.com>
2021-05-24 03:18:16 +0530

    hosting changes

--- dev/u_finder/u_finder/urls.py
+++ dev/u_finder/u_finder/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('students.urls'))
+    path('', include('students.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
 ]
