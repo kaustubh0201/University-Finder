f9d24b1ed522c799be4a2025ef9188fa2c55304d
kaustubh0201 <55658401+kaustubh0201@users.noreply.github.com>
2021-05-24 04:41:03 +0530

    Update settings.py

--- u_finder/settings.py
+++ u_finder/settings.py
@@ -127,6 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
