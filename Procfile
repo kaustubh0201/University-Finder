85337f878aac1fa66548911d004dda6eac7df874
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2021-09-01 13:12:56 +0530

    minor changes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn u_finder.wsgi --log-file -
--- u_finder/urls.py
+++ u_finder/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
-from django.views.static import serve
+# from django.views.static import serve
 from django.conf.urls import url
 
 
@@ -26,8 +26,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('students.urls')),    
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
 ]
 
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
